8bc95e8d9e391c2ad6104f596761759dcac5266c
Mahdi Houshmand <bamarammahdi@gmail.com>
2023-12-15 11:01:13 +0330

    prisma added

--- routes/users.js
+++ routes/users.js
@@ -2,21 +2,31 @@ const { v4: uuidv4 } = require('uuid');
 var express = require('express');
 var router = express.Router();
 
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
 const datacollector = require('../data/datacollector');
 datacollector.init();
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.send(datacollector.users.getAll());
+router.get('/', async function (req, res, next) {
+  // res.send(datacollector.users.getAll());
+  const allUsers = await prisma.user.findMany();
+  res.send(allUsers);
 });
 
 /* POST users listing. */
 router.post('/', async function (req, res, next) {
   const user = req.body;
-  user.id = uuidv4();
-  datacollector.users.add(user);
-  await datacollector.saveChanges();
-  res.send(user);
+  // user.id = uuidv4();
+  // datacollector.users.add(user);
+  const userInDb = await prisma.user.create({
+    data: {
+      name: user.name,
+      email: user.email,
+    },
+  });
+  res.send(userInDb);
 });
 
 /* DELETE user. */
